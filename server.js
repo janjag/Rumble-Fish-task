ab357c82a8ddb36f4740c9436e00298b0acb3035
Jan Jagieła <janjag@humanistcanco.de>
2018-08-12 19:12:40 +0200

    updated html, server and dependencies

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = 'localhost';
 
 // Dummy function for calculating average time of adding a block to the ethereum blockchain
 const calculateTime = () => parseFloat((Math.random(20) *20), 10).toFixed(2);
 
 // App
 const app = express();
-app.use('/dist', express.static(__dirname + '/dist'));
+app.use(cors());
+app.use('/', express.static(__dirname + '/dist'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.get('/api', (req, res) => {
-  res.send(`Average time of adding a block to the ethereum blockchain: \n ${calculateTime()}s.`);
+  const data = {
+    "time": calculateTime()
+  };
+  console.log(data);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.send(JSON.stringify(data));
 });
 
 app.listen(PORT, HOST);
